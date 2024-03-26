4bae864bfb61ba4bbc77f714d89c510cc99413b6
VictorMaona <maonavictor@gmail.com>
2024-03-26 15:47:04 -0700

    updated

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,39 +1,23 @@
 #!/usr/bin/env python3
-"""
-Give some statistics regarding the Nginx logs kept in the MongoDB
-database: logs, nginx, Collection show same as first line such as
-There are x logs and x papers in this collection
-line two: Techniques
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in 5 lines.
-a single line with path=/status and method=GET
-"""
+""" Python with pymongo for MongoDB operations"""
 from pymongo import MongoClient
 
-
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats(mongo_collection, option=None):
-    """
-    Def log_stats(mongo_collection, option=None) as a prototype:
-    Gives some statistics regarding the MongoDB-stored Nginx logs.
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    """ statistics regarding Nginx logs kept in the MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
